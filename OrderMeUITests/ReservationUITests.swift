a7b270c058a8d753146f14d0d5d6d51e88898f89
Adam Cegiełka <adamcegielka@tlen.pl>
2026-01-10 13:05:33 +0100

    add next test with assertion

--- OrderMeUITests/ReservationUITests.swift
+++ OrderMeUITests/ReservationUITests.swift
@@ -13,13 +13,38 @@ final class ReservationUITests: XCTestCase {
     func testExample() throws {
         let app = XCUIApplication()
         app.launch()
-        app.activate()
-        app/*@START_MENU_TOKEN@*/.buttons["loginLaterButton"]/*[[".buttons.containing(.staticText, identifier: \"Login Later\")",".otherElements",".buttons[\"Login Later\"]",".buttons[\"loginLaterButton\"]"],[[[-1,3],[-1,2],[-1,1,1],[-1,0]],[[-1,3],[-1,2]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
-        app/*@START_MENU_TOKEN@*/.staticTexts["1 Kearny St, San Francisco, CA"]/*[[".cells.staticTexts[\"1 Kearny St, San Francisco, CA\"]",".staticTexts[\"1 Kearny St, San Francisco, CA\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
-        app/*@START_MENU_TOKEN@*/.images["qrcode"]/*[[".cells[\"Detect table\"].images",".otherElements.images[\"qrcode\"]",".images[\"qrcode\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
-        app/*@START_MENU_TOKEN@*/.textFields["tableNumberTextField"]/*[[".otherElements",".textFields[\"Table #\"]",".textFields[\"tableNumberTextField\"]",".textFields"],[[[-1,2],[-1,1],[-1,3],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
-        app/*@START_MENU_TOKEN@*/.textFields["tableNumberTextField"]/*[[".otherElements",".textFields[\"3\"]",".textFields[\"Table #\"]",".textFields[\"tableNumberTextField\"]",".textFields"],[[[-1,3],[-1,2],[-1,1],[-1,4],[-1,0,1]],[[-1,3],[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.firstMatch.typeText("3")
-        app/*@START_MENU_TOKEN@*/.staticTexts["Select table"]/*[[".buttons.staticTexts[\"Select table\"]",".staticTexts",".staticTexts[\"Select table\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch.tap()
         
+        app/*@START_MENU_TOKEN@*/.buttons["loginLaterButton"]/*[[".buttons.containing(.staticText, identifier: \"Login Later\")",".otherElements",".buttons[\"Login Later\"]",".buttons[\"loginLaterButton\"]"],[[[-1,3],[-1,2],[-1,1,1],[-1,0]],[[-1,3],[-1,2]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.staticTexts["1 Kearny St, San Francisco, CA"].tap()
+        app/*@START_MENU_TOKEN@*/.images["qrcode"]/*[[".cells[\"Detect table\"].images",".otherElements.images[\"qrcode\"]",".images[\"qrcode\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["tableNumberTextField"]/*[[".otherElements",".textFields[\"Table #\"]",".textFields[\"tableNumberTextField\"]",".textFields"],[[[-1,2],[-1,1],[-1,3],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["tableNumberTextField"]/*[[".otherElements",".textFields[\"3\"]",".textFields[\"Table #\"]",".textFields[\"tableNumberTextField\"]",".textFields"],[[[-1,3],[-1,2],[-1,1],[-1,4],[-1,0,1]],[[-1,3],[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.typeText("3")
+        app/*@START_MENU_TOKEN@*/.staticTexts["Select table"]/*[[".buttons.staticTexts[\"Select table\"]",".staticTexts",".staticTexts[\"Select table\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+    }
+    
+    func testReservationFlow() {
+        let app = XCUIApplication()
+        app.launch()
+
+        let loginLaterButton = app.buttons["loginLaterButton"]
+        XCTAssertTrue(loginLaterButton.exists)
+        loginLaterButton.tap()
+
+        let address = app.staticTexts["1 Kearny St, San Francisco, CA"]
+        XCTAssertTrue(address.waitForExistence(timeout: 5))
+        address.tap()
+
+        let qrImage = app.images["qrcode"]
+        XCTAssertTrue(qrImage.exists)
+        qrImage.tap()
+
+        let tableField = app.textFields["tableNumberTextField"]
+        XCTAssertTrue(tableField.exists)
+        tableField.tap()
+        tableField.typeText("3")
+
+        let selectTable = app.buttons["Select table"]
+        XCTAssertTrue(selectTable.exists)
+        selectTable.tap()
     }
 }
